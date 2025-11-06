3c56fd2d2937d84d4d3a84d3c54e37aa0b6fe885
GongHysin <gonghysin@gmail.com>
2025-11-06 09:09:58 +0800

    Refactor module initialization and training functions to improve support for Chinese characters in matplotlib. Enhance cross-platform compatibility by integrating new font detection features.

---
+++ README.md
@@ -0,0 +1,10 @@
+# 使用步骤
+
+```bash
+
+source /etc/network_turbo
+
+git clone https://github.com/Gonghysin/dive_into_deeplearning_without_d2l.git
+
+```
+
---
+++ modern_CNN/nin.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from modules import load_data_fashion_mnist, train_ch6
+
+def nin_block(in_channels, out_channels, kernel_size, stride, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1),
+        nn.ReLU()
+    )
+
+net = nn.Sequential(
+    nin_block(1, 96, kernel_size=11, stride=4, padding=0),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nin_block(96, 256, kernel_size=5, stride=1, padding=2),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nin_block(256, 384, kernel_size=3, stride=1, padding=1),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nn.Dropout(0.5),
+
+    nin_block(384, 10, kernel_size=3, stride=1, padding=1),
+    nn.AdaptiveAvgPool2d((1, 1)),
+    nn.Flatten()
+)
+
+if __name__ == '__main__':
+    print(net)
+    X = torch.randn(1, 1, 224, 224)
+
+    print("=" * 60)
+    for layer in net:
+        X = layer(X)
+        print(layer.__class__.__name__,'output shape:\t',X.shape)
+
+    lr, num_epochs, batch_size = 0.1, 10, 128
+    train_iter, test_iter = load_data_fashion_mnist(batch_size, resize=224, auto_config=True)
+    train_ch6(net, train_iter, test_iter, num_epochs, lr, device=None)
+
